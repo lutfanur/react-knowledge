69ddd026c970e7d8c8e74b937fa5892bbb1b8d87
Lutfaakter <lutfanur20@gmail.com>
2024-03-14 10:07:16 +0000

    prop types added

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -5,12 +5,12 @@ const Blogs = () => {
 
     useEffect(() => {
         fetch('blogs.json')
-        .then(res => res.json())
-        .then(data => setBlogs(data))
+            .then(res => res.json())
+            .then(data => setBlogs(data))
     }, [])
     return (
-        <div>
-
+        <div className="md:w-2/3">
+            <h1 className="text-4xl ">Blogs: {blogs.length}</h1>
         </div>
     );
 };
